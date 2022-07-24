2b3348b509172442ec5f97f60b6414986eebe511
Lucy Joyce <lucyj20490@gmail.com>
2022-07-24 17:38:39 +0100

    Create NavigationBar using react bootstrap

--- group-7-gfrecipes-react-project/src/NavigationBar.js
+++ group-7-gfrecipes-react-project/src/NavigationBar.js
@@ -1,10 +1,24 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import "./NavigationBar.css";
+import { Link } from "react-router-dom";
 
 function NavigationBar() {
-    return (
-        <div className="navigationBar">
-            <h1>Navigation Bar</h1>
-        </div>
-    )
+  return (
+    <div className="navigationBar">
+      <Navbar expand="lg">
+        <Container>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+          </div>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
 
 export default NavigationBar;
