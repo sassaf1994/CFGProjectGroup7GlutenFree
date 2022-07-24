8bb6a3f30bfe09816dfef6467db54bfafbad9814
Lucy Joyce <lucyj20490@gmail.com>
2022-07-24 20:42:37 +0100

    Update RecipeView and Home components

--- group-7-gfrecipes-react-project/src/Home.js
+++ group-7-gfrecipes-react-project/src/Home.js
@@ -1,27 +1,16 @@
 import SearchBar from "./SearchBar";
 import Header from "./Header";
 import RecipeView from "./RecipeView";
+import Footer from "./Footer";
 
 function Home() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
-      <div className="row">
-        <div className="col-3">
-        <RecipeView/>
-        </div>
-        <div className="col-3">
-        <RecipeView/>
-        </div>
-        <div className="col-3">
-        <RecipeView/>
-        </div>
-        <div className="col-3">
-        <RecipeView/>
-        </div>
-      </div>
-      
+      <RecipeView />
+      <RecipeView />
+      <Footer />
     </div>
   );
 }
--- group-7-gfrecipes-react-project/src/RecipeView.js
+++ group-7-gfrecipes-react-project/src/RecipeView.js
@@ -1,19 +1,62 @@
-import Card from "react-bootstrap/Card"
-import Button from "react-bootstrap/Button"
-import testRecipeImg from "./test-image.png"
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import testRecipeImg from "./test-image.png";
+import "./RecipeView.css";
 
 function RecipeView() {
   return (
-    <Card className="recipeCard" style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={testRecipeImg} />
-      <Card.Body>
-        <Card.Title>Recipe Title</Card.Title>
-        <Card.Text>
-          Recipe Description
-        </Card.Text>
-        <Button variant="primary">Go to recipe</Button>
-      </Card.Body>
-    </Card>
+    <div className="recipeView">
+      <div className="row">
+        <div className="col-3">
+          <div className="recipeCard">
+            <Card className="recipeCard" style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={testRecipeImg} />
+              <Card.Body>
+                <Card.Title>Recipe Title</Card.Title>
+                <Card.Text>Recipe Description</Card.Text>
+                <Button variant="primary">Go to recipe</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="recipeCard">
+            <Card className="recipeCard" style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={testRecipeImg} />
+              <Card.Body>
+                <Card.Title>Recipe Title</Card.Title>
+                <Card.Text>Recipe Description</Card.Text>
+                <Button variant="primary">Go to recipe</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="recipeCard">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={testRecipeImg} />
+              <Card.Body>
+                <Card.Title>Recipe Title</Card.Title>
+                <Card.Text>Recipe Description</Card.Text>
+                <Button variant="primary">Go to recipe</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="recipeCard">
+            <Card className="recipeCard" style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={testRecipeImg} />
+              <Card.Body>
+                <Card.Title>Recipe Title</Card.Title>
+                <Card.Text>Recipe Description</Card.Text>
+                <Button variant="primary">Go to recipe</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
