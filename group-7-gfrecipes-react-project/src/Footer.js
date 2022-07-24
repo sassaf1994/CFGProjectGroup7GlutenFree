9d7e5cb445e5d459a61b79515b0cc296fbb6a643
Lucy Joyce <lucyj20490@gmail.com>
2022-07-24 20:41:59 +0100

    Add Footer component

---
+++ group-7-gfrecipes-react-project/src/Footer.js
@@ -0,0 +1,24 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="row">
+        <div className="col-3">
+          <p>Copyright</p>
+        </div>
+        <div className="col-3">
+          <p>Contact Us</p>
+        </div>
+        <div className="col-3">
+          <p>Socials</p>
+        </div>
+        <div className="col-3">
+          <p>Policy</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
