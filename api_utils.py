293a877dd19a6fa9e09bd9327b1ac1f1ab39eee6
Sarah Assaf <sarah.assaf@hotmail.co.uk>
2022-08-12 08:59:50 +0100

    update to API to include recipe information and start to include account information

--- api_utils.py
+++ api_utils.py
@@ -1,8 +1,12 @@
 import requests
 import re
+from response import response
 from pprint import pprint as pp
 
 endpoint = "https://api.edamam.com/api/recipes/v2"
+
+
+"""FUNCTIONS FOR GETTING RESPONSE"""
 
 
 def recipe_search(query, health):
@@ -26,6 +30,7 @@ def specific_recipe_search(id):
     response = requests.get(endpoint_2, params=payload).json()
     return response
 
+""" FUNCTIONS FOR GETTING GENERAL INFORMATION FROM RESPONSE """
 
 def name_of_recipes(res):
     list_of_names = [recipe["recipe"]["label"] for recipe in res]
@@ -61,6 +66,17 @@ def retrieve_id(res):
     return list_of_ids
 
 
+""" FUNCTIONS FOR GETTING NUTRITIONAL INFORMATION FROM RESPONSE """
+
+
+def nutrition_recipes(res, nutritional_category):
+    list_of_nutrition = [str(int(recipe["recipe"]["totalNutrients"][f"{nutritional_category}"]["quantity"])) + recipe["recipe"]["totalNutrients"][f"{nutritional_category}"]["unit"] for recipe in res]
+    return list_of_nutrition
+
+
+""" FUNCTIONS FOR COMPILING DATA TO SEND NEATLY TO FRONT END """
+
+
 def compile_list_of_results(data):
     index = 0
     list_of_names = name_of_recipes(data)
@@ -69,6 +85,18 @@ def compile_list_of_results(data):
     list_of_ingredients = ingredients_of_recipes(data)
     list_of_image_url = images_url_of_recipes(data)
     list_of_id = retrieve_id(data)
+    list_of_calories = nutrition_recipes(data, "ENERC_KCAL")
+    list_of_total_fat = nutrition_recipes(data, "FAT")
+    list_of_sat_fat = nutrition_recipes(data, "FASAT")
+    list_of_mono_fat = nutrition_recipes(data, "FAMS")
+    list_of_poly_fat = nutrition_recipes(data,"FAPU")
+    list_of_total_carbs = nutrition_recipes(data,"CHOCDF")
+    list_of_net_carbs = nutrition_recipes(data,"CHOCDF.net")
+    list_of_fibre = nutrition_recipes(data,"FIBTG")
+    list_of_sugar = nutrition_recipes(data,"SUGAR")
+    list_of_protein = nutrition_recipes(data,"PROCNT")
+    list_of_salt = nutrition_recipes(data, "NA")
+
     compiled_list_of_ingredients = []
 
     while index < len(name_of_recipes(data)):
@@ -78,7 +106,18 @@ def compile_list_of_results(data):
             "Recipe URL": list_of_recipe_url[index],
             "Ingredients": list_of_ingredients[index],
             "Image URL": list_of_image_url[index],
-            "Recipe ID": list_of_id[index]
+            "Recipe ID": list_of_id[index],
+            "Calories": list_of_calories[index],
+            "Total Fat": list_of_total_fat[index],
+            "Mono Fat": list_of_mono_fat[index],
+            "Poly Fat": list_of_poly_fat[index],
+            "Sat Fat": list_of_sat_fat[index],
+            "Total Carbs": list_of_total_carbs[index],
+            "Net Carbs": list_of_net_carbs[index],
+            "Carbs which Sugar": list_of_sugar[index],
+            "Fibre": list_of_fibre[index],
+            "Protein": list_of_protein[index],
+            "Salt": list_of_salt[index]
                 }
         compiled_list_of_ingredients.append(dictionary)
         index += 1
@@ -95,8 +134,17 @@ def compile_single_result(data):
         "Recipe URL": data["recipe"]["url"],
         "Ingredients": data["recipe"]["ingredientLines"],
         "Image URL": data["recipe"]["images"]["SMALL"]["url"],
-        "Recipe ID": id_number
+        "Recipe ID": id_number,
+        "Calories": str(int(data["recipe"]["totalNutrients"]["ENERC_KCAL"]["quantity"])) + data["recipe"]["totalNutrients"]["ENERC_KCAL"]["unit"],
+        "Total Fat": str(int(data["recipe"]["totalNutrients"]["FAT"]["quantity"])) + data["recipe"]["totalNutrients"]["FAT"]["unit"],
+        "Mono Fat": str(int(data["recipe"]["totalNutrients"]["FAMS"]["quantity"])) + data["recipe"]["totalNutrients"]["FAMS"]["unit"],
+        "Poly Fat": str(int(data["recipe"]["totalNutrients"]["FAPU"]["quantity"])) + data["recipe"]["totalNutrients"]["FAPU"]["unit"],
+        "Sat Fat": str(int(data["recipe"]["totalNutrients"]["FASAT"]["quantity"])) + data["recipe"]["totalNutrients"]["FASAT"]["unit"],
+        "Total Carbs": str(int(data["recipe"]["totalNutrients"]["CHOCDF"]["quantity"])) + data["recipe"]["totalNutrients"]["CHOCDF"]["unit"],
+        "Net Carbs": str(int(data["recipe"]["totalNutrients"]["CHOCDF.net"]["quantity"])) + data["recipe"]["totalNutrients"]["CHOCDF.net"]["unit"],
+        "Carbs which Sugar": str(int(data["recipe"]["totalNutrients"]["SUGAR"]["quantity"])) + data["recipe"]["totalNutrients"]["SUGAR"]["unit"],
+        "Fibre": str(int(data["recipe"]["totalNutrients"]["FIBTG"]["quantity"])) + data["recipe"]["totalNutrients"]["FIBTG"]["unit"],
+        "Protein": str(int(data["recipe"]["totalNutrients"]["PROCNT"]["quantity"])) + data["recipe"]["totalNutrients"]["PROCNT"]["unit"],
+        "Salt": str(int(data["recipe"]["totalNutrients"]["NA"]["quantity"])) + data["recipe"]["totalNutrients"]["NA"]["unit"],
     }
     return dictionary
-
-
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from api_utils import recipe_search, compile_list_of_results, specific_recipe_search, compile_single_result
 from db_utils import insert_new_review, get_specific_review
+from db_utils_user_database import add_user, check_user
 
 app = Flask(__name__)
 
@@ -46,6 +47,28 @@ def post_review():
     for i, j in rating.items():
         insert_new_review(i,j)
     return jsonify(rating)
+
+
+# add verification of email with @
+@app.route("/user/add_user", methods=["POST"])
+def add_user_api():
+    user_to_add = request.get_json()
+    for i, j in user_to_add.items():
+        add_user(i,j)
+    return "Account Created Successfully"
+
+
+# to be completed
+@app.route("/user/verify_user", methods=["GET"])
+def verify_user():
+    user_to_check = request.get_json()
+    for i, j in user_to_check.items():
+        outcome = check_user()
+    return outcome
+
+
+
+
 
 
 
