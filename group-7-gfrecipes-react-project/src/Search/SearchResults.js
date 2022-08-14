c237bc721e23c7f106b8ef9de4b74e2718c58cc4
daniellejayne9 <97840150+daniellejayne9@users.noreply.github.com>
2022-08-14 17:21:26 +0100

    recipe opens in new tab

--- group-7-gfrecipes-react-project/src/Search/SearchResults.js
+++ group-7-gfrecipes-react-project/src/Search/SearchResults.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
-import "./Search.css";
+import "./SearchBar.css";
 import axios from "axios";
 import { ThreeDots } from "react-loader-spinner";
 import RecipeResults from "../RecipeResults/RecipeResults";
